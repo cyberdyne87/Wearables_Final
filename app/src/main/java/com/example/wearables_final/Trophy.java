2f2ae495215ddbed5bb3aa9072bb4c2db51dec6b
Dylan Murphy <dylanm97@ufl.edu>
2021-12-12 12:32:49 -0500

    -Added in hints in the trophy screen to help user find next location

--- app/src/main/java/com/example/wearables_final/Trophy.java
+++ app/src/main/java/com/example/wearables_final/Trophy.java
@@ -14,6 +14,9 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Trophy extends AppCompatActivity {
 
@@ -22,22 +25,48 @@ public class Trophy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trophy);
 
+        TextView hint = findViewById(R.id.nextLocHint);
+        TextView hintHelper = findViewById(R.id.Hintstatic);
+
         Log.d("GPS_Logs", "Current Location: " + Global.currentLocation);
 
         ImageView trophyView = findViewById(R.id.trophyImgView);
-        if (Global.currentLocation == 0) trophyView.setImageResource(trophyroom_graybadges);
+        if (Global.currentLocation == 0)
+        {
+            trophyView.setImageResource(trophyroom_graybadges);
+            hint.setText("Once known as Moon, SD");
+        }
         else if (Global.currentLocation == 1)
+        {
             trophyView.setImageResource(trophyroom_graybadges_color_lunarlander);
+            hint.setText("Old Textile Mill in ME");
+        }
         else if (Global.currentLocation == 2)
+        {
             trophyView.setImageResource(trophyroom_graybadges_color_dodgeweave);
+            hint.setText("Island off of Greece - Water Serpent");
+        }
         else if (Global.currentLocation == 3)
+        {
             trophyView.setImageResource(trophyroom_graybadges_color_contellationdiscoverer);
+            hint.setText("Where Rockets leave Earth");
+        }
         else if (Global.currentLocation == 4)
+        {
             trophyView.setImageResource(trophyroom_graybadges_color_spaceexplorer);
+            hint.setText("Star Gazing Center in the Pacific");
+        }
         else if (Global.currentLocation == 5)
+        {
             trophyView.setImageResource(trophyroom_graybadges_color_eventhorizon);
+            hint.setText("60.1648 , -141.7477");
+        }
         else if (Global.currentLocation == 6)
+        {
             trophyView.setImageResource(trophyroom_graybadges_color_finalfrontier);
+            hintHelper.setText("No Further Assignments");
+            hint.setText("Great Job!");
+        }
 
     }
     /*
